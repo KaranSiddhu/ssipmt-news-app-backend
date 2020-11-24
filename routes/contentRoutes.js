6336aaed20f779a7e03bff25d96f645c85cab735
karan siddhu <karansiddhu22@gmail.com>
2020-11-24 13:57:42 +0530

    Added delete route in Content modal

--- routes/contentRoutes.js
+++ routes/contentRoutes.js
@@ -39,4 +39,22 @@ router.get('/news', (req, res) => {
     });
 });
 
+router.delete('/news/:id', (req, res) => {
+    Content.findByIdAndDelete(req.params.id, (err, docs) => {
+        if (err){
+            res.status(404).json({
+                status:'fail',
+                message:err
+            });
+        }else {
+            res.status(200).json({
+                status:'success',
+                message:`Deleted: ${docs}`
+            });
+        }
+    })
+});
+
+
+
 module.exports = router;
